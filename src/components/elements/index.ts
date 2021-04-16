6eac6e6c506e06ab1b76353255dce24205b7b257
Alan Araujo <alan@estartandodevs.com.br>
2021-04-16 14:10:11 -0300

    Logo component

---
+++ src/components/elements/index.ts
@@ -0,0 +1,6 @@
+import { Button } from "./Button";
+import { Input } from "./Input";
+import { Logo } from "./Logo";
+import { TextArea } from "./TextArea";
+
+export { Button, Input, Logo, TextArea };
