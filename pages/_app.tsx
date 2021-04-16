746c1157a66fb5dd8f6c82d7c4184c9cf16d4478
Alan Araujo <alan@estartandodevs.com.br>
2021-04-16 16:25:30 -0300

    Fix types styled components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import App from "next/app";
+import { ThemeProvider } from "styled-components";
 import { PaymentProvider } from "../src/data/payment/PaymentProvider";
+import { GlobalStyles } from "../src/styles/globalStyled";
+import Theme from "../src/styles/Theme";
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <PaymentProvider>
-        <Component {...pageProps} />
-      </PaymentProvider>
+      <>
+        <ThemeProvider theme={Theme}>
+          <GlobalStyles />
+          <PaymentProvider>
+            <Component {...pageProps} />
+          </PaymentProvider>
+        </ThemeProvider>
+      </>
     );
   }
 }
