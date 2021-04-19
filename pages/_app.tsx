fd01d02fb5392c21e2fb5ff8117927c3d8762db2
Alan Araujo <alan@estartandodevs.com.br>
2021-04-18 23:34:26 -0300

    Fix styled component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,19 +4,19 @@ import { ThemeProvider } from "styled-components";
 import { PaymentProvider } from "../src/data/payment/PaymentProvider";
 import { GlobalStyles } from "../src/styles/globalStyled";
 import Theme from "../src/styles/Theme";
+import { Header } from "../src/components";
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <>
-        <ThemeProvider theme={Theme}>
-          <GlobalStyles />
-          <PaymentProvider>
-            <Component {...pageProps} />
-          </PaymentProvider>
-        </ThemeProvider>
-      </>
+      <ThemeProvider theme={Theme}>
+        <GlobalStyles />
+        <PaymentProvider>
+          <Header />
+          <Component {...pageProps} />
+        </PaymentProvider>
+      </ThemeProvider>
     );
   }
 }
